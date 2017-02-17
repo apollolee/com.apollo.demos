8b617b3916f9a4c8653e8ef96cd299c560eb0c5a
apollolee <apollolee@126.com>
2017-02-17 15:35:04 +0800

    简化代码。

--- code/osgi/base/com.apollo.demos.osgi.base.impl/src/main/scala/com/apollo/demos/osgi/base/impl/thread/NamedCallable.scala
+++ code/osgi/base/com.apollo.demos.osgi.base.impl/src/main/scala/com/apollo/demos/osgi/base/impl/thread/NamedCallable.scala
@@ -7,15 +7,13 @@ import org.slf4j.LoggerFactory.getLogger
 object NamedCallable {
   val logger = getLogger(getClass)
   def apply[T](name: String, callable: Callable[T]) = new NamedCallable(name, callable)
-  private[thread] def name(task: Runnable) = { //TODO 注意：这里依赖JDK的私有实现，目前暂无其他办法解决。
-    try {
-      val callableField = task.getClass.getDeclaredField("callable"); callableField.setAccessible(true)
-      val callable = callableField.get(task)
-      if (callable.isInstanceOf[NamedCallable[_]]) Some(callable.asInstanceOf[NamedCallable[_]].name) else None
+  private[thread] def name(task: Runnable) = try { //TODO 注意：这里依赖JDK的私有实现，目前暂无其他办法解决。
+    val callableField = task.getClass.getDeclaredField("callable"); callableField.setAccessible(true)
+    val callable = callableField.get(task)
+    if (callable.isInstanceOf[NamedCallable[_]]) Some(callable.asInstanceOf[NamedCallable[_]].name) else None
 
-    } catch {
-      case ex: Throwable => logger.warn("Get name is failed.", ex); None
-    }
+  } catch {
+    case ex: Throwable => logger.warn("Get name is failed.", ex); None
   }
 }
 
